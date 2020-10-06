6e44f9fed21bab3c59d3653bca622cae39ae7acf
parkyes90 <parkyes90@gmail.com>
2020-10-06 16:31:34 +0900

    complete dnd

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,34 +1,26 @@
 import React from "react";
-import Square from "./Square";
 import Knight from "./Knight";
 import { canMoveKnight, moveKnight } from "./Game";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import BoardSquare from "./BoardSquare";
 
-function handleSquareClick(toX, toY) {
-  if (canMoveKnight(toX, toY)) {
-    moveKnight(toX, toY);
+function renderPiece(x, y, [knightX, knightY]) {
+  if (x === knightX && y === knightY) {
+    return <Knight />;
   }
+  return <span>E</span>;
 }
 
-function renderSquare(i, [knightX, knightY]) {
+function renderSquare(i, knightPosition) {
   const x = i % 8;
   const y = Math.floor(i / 8);
-  const isKnightHere = x === knightX && y === knightY;
-  const black = (x + y) % 2 === 1;
 
-  const piece = isKnightHere ? (
-    <Knight />
-  ) : (
-    <span style={{ color: black ? "black" : "white", textAlign: "center" }}>
-      E
-    </span>
-  );
   return (
-    <div
-      key={i}
-      style={{ width: "12.5%", height: "12.5%" }}
-      onClick={() => handleSquareClick(x, y)}
-    >
-      <Square black={black}>{piece}</Square>
+    <div key={i} style={{ width: "12.5%", height: "12.5%" }}>
+      <BoardSquare x={x} y={y}>
+        {renderPiece(x, y, knightPosition)}
+      </BoardSquare>
     </div>
   );
 }
@@ -39,15 +31,17 @@ export default function Board({ knightPosition }) {
     squares.push(renderSquare(i, knightPosition));
   }
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        flexWrap: "wrap",
-      }}
-    >
-      {squares}
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+        }}
+      >
+        {squares}
+      </div>
+    </DndProvider>
   );
 }
